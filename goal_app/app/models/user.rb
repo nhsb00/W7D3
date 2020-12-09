1b2f93f043bdb3b816b3a5244502b3ab871c114b
soobin <nhsb00@hotmail.com>
2020-12-09 16:12:44 -0500

    finish with model's spec

--- goal_app/app/models/user.rb
+++ goal_app/app/models/user.rb
@@ -34,11 +34,11 @@ class User < ApplicationRecord
     end
 
     def reset_session_token!
-        self.update!(session_token: generate_session_token)
+        self.update!(session_token: self.class.generate_session_token)
         
         self.session_token
 
-        # self.session_token = generate_session_token 
+        # self.session_token = self.class.generate_session_token # class method (self.class or User)
         # self.save! 
     end
 
--- goal_app/spec/models/user_spec.rb
+++ goal_app/spec/models/user_spec.rb
@@ -29,21 +29,21 @@ RSpec.describe User, type: :model do
         # result has to be :user
         # user.username
         it "should find the same user" do
-        expect(user.find_by_credentials('#{user.username}', 'onering')).to eq(user)
+        expect(User.find_by_credentials("#{user.username}", 'onering')).to eq(user)
         end
       end
 
       context "finds the wrong user" do
         # result is not :user
         it "should not find the user" do
-        expect(user.find_by_credentials('#{user.username}', 'onering')).not_to eq(user)
+        expect(User.find_by_credentials("Philip Kor", 'onering')).not_to eq(user)
         end
       end
 
       # context where username is correct but password is wrong
       context "finds user but wrong password" do
         it "should not return user as password given was wrong" do
-          expect(user.find_by_credentials('#{user.username}', 'tworing')).not_to eq(user)
+          expect(User.find_by_credentials("#{user.username}", 'tworing')).not_to eq(user)
         end
       end
       
